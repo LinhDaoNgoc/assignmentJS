02c39aacc0ee3c18d4d9a40c3d064d41b57850b1
DaoNgocLinh <daongoclinh22@gmail.com>
2022-01-10 12:59:34 +0700

    update src

--- src/main.js
+++ src/main.js
@@ -1,13 +1,47 @@
 import Navigo from "navigo";
+import HomePage from "./pages/home";
 
-const router = new Navigo("/");
+const router = new Navigo("/", { linksSelector: "a" });
+
+const print = (content) => {
+    document.getElementById("app").innerHTML = content;
+};
 
 router.on({
-  "/": () => {
-    console.log("Home Page");
-  },
-  "/about": () => {
-    console.log("About Page");
-  },
+    "/": () => {
+        print(HomePage.render());
+    },
+    "/about": () => {
+        print("About Page");
+    },
+    "/product": () => {
+
+    },
 });
 router.resolve();
+
+// class KhuanBanh {
+//     constructor(luongDuong, luongBot) {
+//         this.luongDuong = luongDuong;
+//         this.luongBot = luongBot;
+//     }
+
+//     showInfo() {
+//         console.log(`
+//             Đường: ${this.luongDuong}
+//             Bột: ${this.luongBot}
+//         `);
+//     }
+// }
+
+// const banhDeo = new KhuanBanh(10, 20);
+// const banhNuong = new KhuanBanh(20, 30);
+
+// console.log(banhDeo.luongDuong);
+// banhDeo.showInfo();
+// banhNuong.showInfo();
+
+// function sum(a,b){
+// const numberA = a;
+// }
+// sum(3,4)
