960d70a138d1a43fb5f974cffcb3f2c69d19831e
DaoNgocLinh <daongoclinh22@gmail.com>
2022-01-12 14:21:00 +0700

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,3 @@
-// const Header = {
-//     render() {
-//         return /* html */ `
-//       <div class="bg-blue-800 py-4">
-//       <a href="">
-//         <img src="https://picsum.photos/150/40" alt="" class="mx-auto">
-//       </a>
-//     </div>
-//     <div class="bg-orange-500">
-//       <ul class="flex">
-//         <li><a href="/" class="block px-4 py-3 hover:bg-indigo-500 hover:text-white">Trang chủ </a></li>
-//         <li><a href="/about" class="block px-4 py-3 hover:bg-indigo-500 hover:text-white">Tyển sinh</a></li>
-//         <li><a href="/news" class="block px-4 py-3 hover:bg-indigo-500 hover:text-white">News</a></li>
-//         <li><a href="/contact" class="block px-4 py-3 hover:bg-indigo-500 hover:text-white">Contact</a></li>
-//       </ul>
-//     </div>
-//       `;
-//     },
-// };
-// export default Header;
 const Header = {
     render() {
         return /* html */`    <header>
